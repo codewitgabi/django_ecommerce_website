e54f73cc98b4a065d6019a862b797e3f2b888783
codewitgabi <gabrielmicheal497@gmail.com>
2023-01-28 23:39:19 +0100

    Updated models

--- products/models.py
+++ products/models.py
@@ -9,16 +9,18 @@ class Category(models.Model):
 		
 	class Meta:
 		verbose_name_plural = "Categories"
+		
 
 class Product(models.Model):
 	name = models.CharField(max_length= 100)
 	price = models.DecimalField(max_digits= 10, decimal_places= 2, default= 0)
 	image = models.ImageField(upload_to= "products_image/")
-	category = models.ForeignKey("Category", on_delete= models.SET_NULL, null= True, blank= True)
+	category = models.ForeignKey(Category, on_delete= models.SET_NULL, null= True, blank= True)
 	
 	
 	def __str__(self):
 		return self.name
+		
 		
 class Order(models.Model):
 	customer = models.ForeignKey(User, on_delete= models.SET_NULL, null= True, blank= True)
@@ -69,3 +71,4 @@ class ShippingInfo(models.Model):
 		
 	class Meta:
 		verbose_name_plural = "ShippingInfo"
+
