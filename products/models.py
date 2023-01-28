3952a8df8926194cb2ceebf4b2fd05529dd77846
Shagbaor Agber(Pyman) <67190277+Codewithshagbaor@users.noreply.github.com>
2023-01-28 22:15:54 +0100

    Category comes before Product

--- products/models.py
+++ products/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from accounts.models import User
 	
+class Category(models.Model):
+	name = models.CharField(max_length= 30)
 	
+	def __str__(self):
+		return self.name
+		
+	class Meta:
+		verbose_name_plural = "Categories"
+
 class Product(models.Model):
 	name = models.CharField(max_length= 100)
 	price = models.DecimalField(max_digits= 10, decimal_places= 2, default= 0)
@@ -11,17 +19,6 @@ class Product(models.Model):
 	
 	def __str__(self):
 		return self.name
-		
-		
-class Category(models.Model):
-	name = models.CharField(max_length= 30)
-	
-	def __str__(self):
-		return self.name
-		
-	class Meta:
-		verbose_name_plural = "Categories"
-		
 		
 class Order(models.Model):
 	customer = models.ForeignKey(User, on_delete= models.SET_NULL, null= True, blank= True)
